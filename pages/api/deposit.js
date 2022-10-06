71b46b00a48c716eb732af39190fdeaeb7d01a8d
sirinnicha <sirinnicha.ta@gmail.com>
2022-10-06 16:01:03 +0700

    committ

--- pages/api/deposit.js
+++ pages/api/deposit.js
@@ -1,11 +1,16 @@
 import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB, writeUserDB } from "../../backendLibs/dbLib";
 
 export default function depositRoute(req, res) {
   if (req.method === "PUT") {
     //check authentication
     const user = checkToken(req);
     // return res.status(403).json({ ok: false, message: "You do not have permission to deposit" });
-
+    if (!user || user.isAdmin === true) {
+      return res
+        .status(403)
+        .json({ ok: false, message: "You do not have permission to deposit" });
+    }
     const amount = req.body.amount;
     //validate body
     if (typeof amount !== "number")
@@ -13,9 +18,17 @@ export default function depositRoute(req, res) {
 
     //check if amount < 1
     // return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
-
+    if (amount < 1) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Amount must be greater than 0" });
+    }
     //find and update money in DB
-
+    const users = readUsersDB();
+    const userIndex = users.findIndex((x) => user.username === x.username);
+    users[userIndex].money += amount;
+    writeUserDB(users);
+    res.json({ ok: true, money: users[userIndex].money });
     //return response
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -2,7 +2,12 @@ export default function withdrawRoute(req, res) {
   if (req.method === "PUT") {
     //check authentication
     //return res.status(403).json({ ok: false, message: "You do not have permission to withdraw" });
-
+    const user = checkToken(req);
+    if (!user || user.isAdmin) {
+      return res
+        .status(403)
+        .json({ ok: false, message: "You do not have permission to withdraw" });
+    }
     const amount = req.body.amount;
     //validate body
     if (typeof amount !== "number")
@@ -10,10 +15,26 @@ export default function withdrawRoute(req, res) {
 
     //check if amount < 1
     // return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
-
+    if (amount < 1) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Amount must be greater than 0" });
+    }
     //find and update money in DB (if user has enough money)
     //return res.status(400).json({ ok: false, message: "You do not has enough money" });
-
+    const users = readUsersDB();
+    const foundUser = users.find((x) => x.username === user.username);
+    if (foundUser.money - amount < 0) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "You do not has enough money" });
+    }
+    foundUser.money -= amount;
+    writeUsersDB(users);
+    return res.json({
+      ok: true,
+      money: foundUser.money,
+    });
     //return response
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
